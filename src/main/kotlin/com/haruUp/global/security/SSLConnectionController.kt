f0b73da2dfd4cc4b4be332ae4e0727a83f447074
BJW <backj123@naver.com>
2025-12-27 01:42:34 +0900

    [BJW] 2025/12/27 SSL Connection test - 3

--- src/main/kotlin/com/haruUp/global/security/SSLConnectionController.kt
+++ src/main/kotlin/com/haruUp/global/security/SSLConnectionController.kt
@@ -1,13 +1,15 @@
 package com.haruUp.global.security
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/actuator")
+@RestController
+@RequestMapping("/actuator")
 class SSLConnectionController {
 
-    @RequestMapping("/health")
-    fun sslConnection() : String{
-       return "OK";
+    @GetMapping("/health")
+    fun health(): String {
+        return "OK"
     }
 }
